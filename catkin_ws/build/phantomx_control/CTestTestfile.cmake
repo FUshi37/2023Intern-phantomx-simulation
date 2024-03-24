85cbeecae30ccaf39b84e8b9bf31b5d4fce437ec
Zhe Yang <89384480+FUshi37@users.noreply.github.com>
2024-03-24 17:55:17 +0800

    Delete catkin_ws directory

--- catkin_ws/build/phantomx_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yangzhe/Intern/simulation/examples/phantomx_gazebo/catkin_ws/src/phantomx_control
-# Build directory: /home/yangzhe/Intern/simulation/examples/phantomx_gazebo/catkin_ws/build/phantomx_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
